af6e2874a819d92e15a68a86ffee25ff6d425afe
looping42 <loopingr42@gmail.com>
2015-08-09 20:16:19 +0200

    ok2

--- JavaCVSite/src/javaCVSite/JavaCVSiteServlet.java
+++ JavaCVSite/src/javaCVSite/JavaCVSiteServlet.java
@@ -1,10 +1,14 @@
 package javaCVSite;
+import com.googlecode.objectify.ObjectifyService;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class JavaCVSiteServlet extends HttpServlet {
+	static {
+	        ObjectifyService.register(Personnage.class); // Fait connaître votre classe-entité à Objectify
+	}
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, Denis");
